74b4c3b60ad83dd4d5aaf0c645e9ff9eb96d4d37
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-05-29 11:04:51 +0300

    Sort by price/popularity

--- assets/js/browser-action.js
+++ assets/js/browser-action.js
@@ -1,16 +1,25 @@
 $(function () {
     // Get initial stored toggle value
     chrome.storage.sync.get({
-        enableSortByPopularity: true
+        enableSortByPopularity: true,
+        enableSortByPrice: false
     }, function(items) {
         // Set checkbox checked value based on stored toggle value
         $('#enable-sort-by-popularity').prop('checked', items.enableSortByPopularity);
+        $('#enable-sort-by-price').prop('checked', items.enableSortByPrice);
     });
 
     $('#enable-sort-by-popularity').change(function(){
         // Update stored toggle value using checkbox checked property
         chrome.storage.sync.set({
             enableSortByPopularity: $('#enable-sort-by-popularity').prop('checked')
+        });
+    });
+
+    $('#enable-sort-by-price').change(function(){
+        // Update stored toggle value using checkbox checked property
+        chrome.storage.sync.set({
+            enableSortByPrice: $('#enable-sort-by-price').prop('checked')
         });
     });
 });
--- assets/js/content.js
+++ assets/js/content.js
@@ -2,7 +2,8 @@
 $(document).ready(function () {
     // Get on/off toggle value for this feature
     chrome.storage.sync.get({
-        enableSortByPopularity: true
+        enableSortByPopularity: true,
+        enableSortByPrice: false
     }, function (options) {
         // Check whether this feature is disabled
         if (!options.enableSortByPopularity) {
@@ -13,29 +14,41 @@ $(document).ready(function () {
         var results = [];
 
         // Traverse search results
-        $('li[listingid]:has(.hotness-signal.red)').each(function () {
+        $('li[listingid]:has(.hotness-signal)').each(function () {
             // Convert to jQuery object
             var listing = $(this);
 
+            if (listing.find('.hotness-signal:contains(" sold")').length) {
             // Default listing sold count to 0
             var soldCount = 0;
-			
-            var divText = listing.find('.hotness-signal.red').text().trim();
 
             // Get sold count as integer
-            soldCount = divText.endsWith(' sold') ? (parseInt(divText) || 0) : 0;
+            soldCount = parseInt(listing.find('.hotness-signal:contains(" sold")').text()) || 0;
+
+            if (options.enableSortByPrice) {
+                var feeEl = listing.find('.fee');
+
+                soldCount = (parseFloat(listing.find('.lvprice.prc .bold').text().replace(',', ''))+(feeEl.length ? parseFloat(listing.find('.fee').text().replace(',', '')) : 0))/soldCount || 0;
+
+                listing.find('.lvprice.prc .bold').after('<div class="cmpat">Avg: '+soldCount+'</div>');
+            }
 
             // Add item sold count and listing itself
             results.push({ sold: soldCount, listing: listing });
 
             // Delete element temporarily
             listing.remove();
+            }
         });
 
         // Sort all listings by sold count DESC
-        results.sort(function (a, b) {
+        var sortFunc = function (a, b) {
             return b.sold - a.sold;
-        });
+        };
+        if (options.enableSortByPrice) sortFunc = function (a, b) {
+            return a.sold - b.sold;
+        };
+        results.sort(sortFunc);
 
         // Get search results parent list
         var ul = $('ul#ListViewInner');
@@ -48,5 +61,11 @@ $(document).ready(function () {
         // Warn the user about the modified result order
         ul.prepend('<li><hr /></li>');
         ul.prepend('<li>These search results have been modified by <b>eBay™ Popularity Sort</b>.</li>');
+
+        $('li[listingid]').each(function () {
+            var listing = $(this);
+            var feeEl = listing.find('.fee');
+            listing.find('.lvprice.prc .bold').text(parseFloat(listing.find('.lvprice.prc .bold').text().replace(',', ''))+(feeEl.length ? parseFloat(listing.find('.fee').text().replace(',', '')) : 0));
+        });
     });
 });
