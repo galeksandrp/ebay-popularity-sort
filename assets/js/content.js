ae920e34f62d29ae5a3fc13f430f7dad968c2940
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-07-04 20:26:58 +0300

    Migrate to new eBay HTML results format

--- assets/js/content.js
+++ assets/js/content.js
@@ -14,36 +14,37 @@ $(document).ready(function () {
         var results = [];
 
         // Traverse search results
-        $('li[listingid]'+(options.enableSortByPopularity.endsWith('-all') ? '' : ':has(.hotness-signal)')).each(function () {
+        $('li[listingid]'+(options.enableSortByPopularity.endsWith('-all') ? '' : ':has(.hotness-signal)')+', .s-item'+(options.enableSortByPopularity.endsWith('-all') ? '' : ':has(.s-item__itemHotness)')).each(function () {
             // Convert to jQuery object
             var listing = $(this);
 
-            if (options.enableSortByPopularity.endsWith('-all') || listing.find('.hotness-signal:contains(" Sold")').length) {
+            if (options.enableSortByPopularity.endsWith('-all') || listing.find('.hotness-signal:contains(" Sold"), .s-item__itemHotness:contains(" Sold")').length) {
             if (options.enableSortByPopularity !== 'disable-sort') {
             // Default listing sold count to 0
             var soldCount = 0;
 
             // Get sold count as integer
             if (options.enableSortByPopularity === 'enable-sort-by-rating-all') {
-                soldCount = parseInt($(listing.find('.selrat')[0]).text().replace(/[(,)]/g, '')) || 0;
+                soldCount = parseInt($(listing.find('.selrat, .s-item__seller-info-text')[0]).text().replace(/[(,)]/g, '')) || 0;
             } else if (options.enableSortByPopularity === 'enable-sort-by-positive-all') {
-                soldCount = parseFloat($(listing.find('.selrat')[1]).text()) || 0;
+                soldCount = parseFloat($(listing.find('.selrat, .s-item__seller-info-text')[1]).text()) || 0;
             } else {
-                soldCount = parseInt(listing.find('.hotness-signal:contains(" Sold")').text()) || 0;
+                soldCount = parseInt(listing.find('.hotness-signal:contains(" Sold"), .s-item__itemHotness:contains(" Sold")').text().replace(',', '')) || 0;
             }
 
             if (options.enableSortByPopularity === 'enable-sort-by-price') {
-                var feeEl = listing.find('.fee');
+                var feeEl = listing.find('.fee, .s-item__shipping');
+                var fee = feeEl.length ? (feeEl.text().trim() === 'Free International Shipping' ? 0 : parseFloat(feeEl.text().replace(',', ''))) : 0;
 
-                var price = options.splitByQuantity ? (listing.find('.lvtitle').text().match(/(?: |^)[0-9]+x/i) ? parseFloat(listing.find('.lvprice.prc .bold').text().replace(',', ''))/listing.find('.lvtitle').text().match(/(?: |^)([0-9]+)x/i)[1] : parseFloat(listing.find('.lvprice.prc .bold').text().replace(',', ''))) : (parseFloat(listing.find('.lvprice.prc .bold').text().replace(',', '')));
+                var price = options.splitByQuantity ? (listing.find('.lvtitle, .s-item__title').text().match(/(?: |^)[0-9]+x/i) ? parseFloat(listing.find('.lvprice.prc .bold, .s-item__price').text().replace(',', ''))/listing.find('.lvtitle, .s-item__title').text().match(/(?: |^)([0-9]+)x/i)[1] : parseFloat(listing.find('.lvprice.prc .bold, .s-item__price').text().replace(',', ''))) : (parseFloat(listing.find('.lvprice.prc .bold, .s-item__price').text().replace(',', '')));
 
-                soldCount = (price+(feeEl.length ? parseFloat(listing.find('.fee').text().replace(',', '')) : 0))/soldCount || 0;
+                soldCount = (price+fee)/soldCount || 0;
 
-                listing.find('.lvprice.prc .bold').after('<div class="cmpat">Avg: '+soldCount+'</div>');
+                listing.find('.lvprice.prc .bold, .s-item__price').after('<div class="cmpat">Avg: '+soldCount+'</div>');
             } else if (options.enableSortByPopularity === 'enable-sort-by-average-all') {
-                soldCount = parseInt($(listing.find('.selrat')[0]).text().replace(/[(,)]/g, ''))*(parseFloat($(listing.find('.selrat')[1]).text())/100) || 0;
+                soldCount = parseInt($(listing.find('.selrat, .s-item__seller-info-text')[0]).text().replace(/[(,)]/g, ''))*(parseFloat($(listing.find('.selrat, .s-item__seller-info-text')[1]).text())/100) || 0;
 
-                listing.find('.lvprice.prc .bold').after('<div class="cmpat">Avg: '+soldCount+'</div>');
+                listing.find('.lvprice.prc .bold, .s-item__price').after('<div class="cmpat">Avg: '+soldCount+'</div>');
             }
             }
             // Add item sold count and listing itself
@@ -64,7 +65,7 @@ $(document).ready(function () {
         if (options.enableSortByPopularity !== 'disable-sort') results.sort(sortFunc);
 
         // Get search results parent list
-        var ul = $('ul#ListViewInner');
+        var ul = $('ul#ListViewInner, .srp-results');
 
         // Re-add the sorted results
         ul.prepend(results.map(function(item){
@@ -75,10 +76,11 @@ $(document).ready(function () {
         ul.prepend('<li><hr /></li>');
         ul.prepend('<li>These search results have been modified by <b>eBay™ Popularity Sort</b>.</li>');
 
-        $('li[listingid]').each(function () {
+        $('li[listingid], .s-item').each(function () {
             var listing = $(this);
-            var feeEl = listing.find('.fee');
-            listing.find('.lvprice.prc .bold').text(parseFloat(listing.find('.lvprice.prc .bold').text().replace(',', ''))+(feeEl.length ? parseFloat(listing.find('.fee').text().replace(',', '')) : 0));
+            var feeEl = listing.find('.fee, .s-item__shipping');
+            var fee = feeEl.length ? (feeEl.text().trim() === 'Free International Shipping' ? 0 : parseFloat(feeEl.text().replace(',', ''))) : 0;
+            listing.find('.lvprice.prc .bold, .s-item__price').text(parseFloat(listing.find('.lvprice.prc .bold, .s-item__price').text().replace(',', ''))+fee);
         });
     });
 });
